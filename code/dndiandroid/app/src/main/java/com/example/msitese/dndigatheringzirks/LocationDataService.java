a17dbd1e7c258817f528054a4e88ed2df2bc8128
Rajat Deepak Mathur <rajatdem@andrew.cmu.edu>
2017-06-09 18:25:33 -0400

    [rajatdem] Added new package for gathering zirk

---
+++ code/dndiandroid/app/src/main/java/com/example/msitese/dndigatheringzirks/LocationDataService.java
@@ -0,0 +1,22 @@
+package com.example.msitese.dndigatheringzirks;
+
+/**
+ * Created by rajatmathur on 6/9/17.
+ */
+
+/* Create object of DNDIFramework and use its functions
+ *  Use the ConfigService
+ *  Use the CommandEvent for event modes
+ *
+ */
+
+public class LocationDataService {
+
+    //TODO: Capture the Location Data from the Phone
+
+
+
+    double a = 10;
+
+
+}
