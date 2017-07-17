701905fcdc36458084d384da8c3b534bb6f3e77b
Rishabh <Rishabh.Gulati@us.bosch.com>
2017-07-17 11:12:38 -0400

    fix duplicate middleware events

--- code/dndiandroid/app/src/main/java/edu/cmu/msitese/dndiandroid/frameworkinterface/ZirkManagerService.java
+++ code/dndiandroid/app/src/main/java/edu/cmu/msitese/dndiandroid/frameworkinterface/ZirkManagerService.java
@@ -15,6 +15,9 @@ import com.bezirk.middleware.android.BezirkMiddleware;
 import com.bezirk.middleware.messages.Event;
 import com.bezirk.middleware.messages.EventSet;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import edu.cmu.msitese.dndiandroid.datagathering.gps.LocationDataService;
 import edu.cmu.msitese.dndiandroid.datagathering.twitter.TwitterService;
 import edu.cmu.msitese.dndiandroid.datainference.keyword.KeywordMatchService;
@@ -29,27 +32,36 @@ import edu.cmu.msitese.dndiandroid.event.ResultEvent;
 public class ZirkManagerService extends Service {
 
     private static final String TAG = "ZIRK";
-    public static final String ACTION = "edu.cmu.msitese.dndiandroid.ZirkManagerService";
+
+    // Depending on the device and the current load on the device, Bezirk middleware could take
+    // anywhere between 300-1500 ms to initialize completely. This delay (in ms) is used to ensure
+    // that the initialization has completed before zirks are registered with the middleware.
+    private static final int BEZIRK_INITIALIZATION_DELAY = 1500;
+    public static final String ACTION = "edu.cmu.msitese.dndiandroid.ConfigService";
 
     private Bezirk bezirk;
     private final EventSet eventSet = new EventSet(
             ResultEvent.class
     );
 
-    private final IBinder mBinder = new ZirkManagerServiceBinder();
-    private final Class<?> [] services = {
+    private final IBinder mBinder = new ConfigServiceBinder();
+    private final Class<?>[] services = {
             TwitterService.class,
             LocationDataService.class,
-            GeocodingService.class
-            KeywordMatchService.class,
+            GeocodingService.class,
+            KeywordMatchService.class
     };
 
-    /** Called when the service is being created. */
+    /**
+     * Called when the service is being created.
+     */
     @Override
     public void onCreate() {
 
+        Log.wtf(TAG, "STARTING OTHER SERVICES");
         // initialize the Bezirk service
         BezirkMiddleware.initialize(getBaseContext());
+
 
         // register with Bezirk middleware to get an instance of Bezirk API.
         bezirk = BezirkMiddleware.registerZirk("ZirkManager");
@@ -59,22 +71,39 @@ public class ZirkManagerService extends Service {
 
             @Override
             public void receiveEvent(Event event, ZirkEndPoint zirkEndPoint) {
+        // initialize the zirk after the middleware has initialized
+        new Timer().schedule(new ZirkInitializer(), BEZIRK_INITIALIZATION_DELAY);
+    }
 
-                if(event instanceof ResultEvent){
-                    final ResultEvent resultEvent = (ResultEvent) event;
-                    Intent intent = new Intent(ACTION);
-                    intent.putExtra("result", DNDIFramework.KEYWORD_MATCH);
-                    intent.putStringArrayListExtra("keywords", resultEvent.getMatchList());
-                    LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent);
+
+    private class ZirkInitializer extends TimerTask {
+        @Override
+        public void run() {
+            // register with Bezirk middleware to get an instance of Bezirk API.
+            bezirk = BezirkMiddleware.registerZirk("ConfigZirk");
+
+            eventSet.setEventReceiver(new EventSet.EventReceiver() {
+
+                @Override
+                public void receiveEvent(Event event, ZirkEndPoint zirkEndPoint) {
+
+                    if (event instanceof ResultEvent) {
+                        final ResultEvent resultEvent = (ResultEvent) event;
+                        Intent intent = new Intent(ACTION);
+                        intent.putExtra("result", DNDIFramework.KEYWORD_MATCH);
+                        intent.putStringArrayListExtra("keywords", resultEvent.getMatchList());
+                        LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast
+                                (intent);
+                    }
                 }
-            }
-        });
-        bezirk.subscribe(eventSet);
+            });
+            bezirk.subscribe(eventSet);
 
-        for(Class<?> cls: services){
-            startService(new Intent(getBaseContext(), cls));
+            for (Class<?> cls : services) {
+                startService(new Intent(getBaseContext(), cls));
+            }
+            Log.i(TAG, this.getClass().getName() + ":: wait...");
         }
-        Log.i(TAG, this.getClass().getName() + ":: wait...");
     }
 
     @Override
@@ -88,7 +117,7 @@ public class ZirkManagerService extends Service {
         return mBinder;
     }
 
-    public void sendBezirkEvent(Event evt){
+    public void sendBezirkEvent(Event evt) {
         bezirk.sendEvent(evt);
     }
 
@@ -105,23 +134,23 @@ public class ZirkManagerService extends Service {
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
-    public void setBezirkInstance(Bezirk bezirk){
+    public void setBezirkInstance(Bezirk bezirk) {
         this.bezirk = bezirk;
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
-    public void setEventSetSubscriptionHandler(EventSet.EventReceiver receiver){
+    public void setEventSetSubscriptionHandler(EventSet.EventReceiver receiver) {
         eventSet.setEventReceiver(receiver);
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
-    public EventSet.EventReceiver getEventSetSubscriptionHandler(){
+    public EventSet.EventReceiver getEventSetSubscriptionHandler() {
         return eventSet.getEventReceiver();
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
-    public void stopChildrenService(){
-        for(Class<?> cls: services){
+    public void stopChildrenService() {
+        for (Class<?> cls : services) {
             stopService(new Intent(getBaseContext(), cls));
         }
     }
